c2ce74e3f321a3171bb3c3c1b55ada0454ddd115
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-11-08 20:14:54 +0530

    Create sumoftwono.java

---
+++ sumoftwono.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in); 
+	    int a= sc.nextInt();  
+	    int b= sc.nextInt();  
+	    System.out.format("%d %d %d",a,b,a+b);
+	}
+}
+/*
+4
+5
+9*/
